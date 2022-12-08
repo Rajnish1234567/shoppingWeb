6a723449901e15cc2f9a0513272d0affe92ee54d
Rajnish1234567 <rajnish0147r@gmail.com>
2022-12-08 12:12:25 +0530

    adding shopping list

--- project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe} from '../recipe.model'
 export class RecipeListComponent {
   recipes: Recipe[] = [
     new Recipe("Random Recipe","Recipe description","https://dynaimage.cdn.cnn.com/cnn/q_auto,w_634,c_fill,g_auto,h_357,ar_16:9/http%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F221118094527-01-body-indonesia-famous-dishes-beef-rendang.jpg"),
+    new Recipe("Random Recipe","Recipe description","https://dynaimage.cdn.cnn.com/cnn/q_auto,w_634,c_fill,g_auto,h_357,ar_16:9/http%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F221118094527-01-body-indonesia-famous-dishes-beef-rendang.jpg")  
   ];
 
   constructor() {}
